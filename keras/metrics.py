a2cba685585be2d6d8c2a1feb9d967231f305d94
A. Unique TensorFlower <gardener@tensorflow.org>
2021-10-28 12:21:28 -0700

    Keras IoU Metrics.
    1. Refactor `MeanIoU` to inherit `_BaseIoU` which only updates the confusion matrix and can be used as the base class for several `IoU` metrics.
    
    2. Implement `IoU`.
    
    PiperOrigin-RevId: 406194092

--- keras/metrics.py
+++ keras/metrics.py
@@ -2998,8 +2998,173 @@ class KLDivergence(MeanMetricWrapper):
         kullback_leibler_divergence, name, dtype=dtype)
 
 
+class _BaseIOU(Metric):
+  """Computes the confusion matrix for Intersection-Over-Union metrics.
+
+  Intersection-Over-Union is a common evaluation metric for semantic image
+  segmentation. IOU is defined as follows:
+    IOU = true_positive / (true_positive + false_positive + false_negative).
+  The predictions are accumulated in a confusion matrix, weighted by
+  `sample_weight` and the metric is then calculated from it.
+
+  If `sample_weight` is `None`, weights default to 1.
+  Use `sample_weight` of 0 to mask values.
+
+  Args:
+    num_classes: The possible number of labels the prediction task can have.
+      This value must be provided, since a confusion matrix of dimension =
+      [num_classes, num_classes] will be allocated.
+    name: (Optional) string name of the metric instance.
+    dtype: (Optional) data type of the metric result.
+  """
+
+  def __init__(self, num_classes, name=None, dtype=None):
+    super(_BaseIOU, self).__init__(name=name, dtype=dtype)
+    self.num_classes = num_classes
+
+    # Variable to accumulate the predictions in the confusion matrix.
+    self.total_cm = self.add_weight(
+        'total_confusion_matrix',
+        shape=(num_classes, num_classes),
+        initializer=tf.compat.v1.zeros_initializer)
+
+  def update_state(self, y_true, y_pred, sample_weight=None):
+    """Accumulates the confusion matrix statistics.
+
+    Args:
+      y_true: The ground truth values.
+      y_pred: The predicted values.
+      sample_weight: Optional weighting of each example. Defaults to 1. Can be a
+        `Tensor` whose rank is either 0, or the same rank as `y_true`, and must
+        be broadcastable to `y_true`.
+
+    Returns:
+      Update op.
+    """
+
+    y_true = tf.cast(y_true, self._dtype)
+    y_pred = tf.cast(y_pred, self._dtype)
+
+    # Flatten the input if its rank > 1.
+    if y_pred.shape.ndims > 1:
+      y_pred = tf.reshape(y_pred, [-1])
+
+    if y_true.shape.ndims > 1:
+      y_true = tf.reshape(y_true, [-1])
+
+    if sample_weight is not None:
+      sample_weight = tf.cast(sample_weight, self._dtype)
+      if sample_weight.shape.ndims > 1:
+        sample_weight = tf.reshape(sample_weight, [-1])
+
+    # Accumulate the prediction to current confusion matrix.
+    current_cm = tf.math.confusion_matrix(
+        y_true,
+        y_pred,
+        self.num_classes,
+        weights=sample_weight,
+        dtype=self._dtype)
+    return self.total_cm.assign_add(current_cm)
+
+  def reset_state(self):
+    backend.set_value(
+        self.total_cm, np.zeros((self.num_classes, self.num_classes)))
+
+
+@keras_export('keras.metrics.IoU')
+class IoU(_BaseIOU):
+  """Computes the Intersection-Over-Union metric for a specific class.
+
+  Intersection-Over-Union is a common evaluation metric for semantic image
+  segmentation, which is defined as follows:
+    IOU = true_positive / (true_positive + false_positive + false_negative).
+  The predictions are accumulated in a confusion matrix, weighted by
+  `sample_weight` and the metric is then calculated from it.
+
+  If `sample_weight` is `None`, weights default to 1.
+  Use `sample_weight` of 0 to mask values.
+
+  Args:
+    num_classes: The possible number of labels the prediction task can have.
+      A confusion matrix of dimension = [num_classes, num_classes] will be
+      allocated to accumulate predictions from which the metric is calculated.
+    target_class_ids: A tuple or list of target class ids for which the metric
+      is returned.
+    name: (Optional) string name of the metric instance.
+    dtype: (Optional) data type of the metric result.
+
+  Standalone usage:
+
+  >>> # cm = [[1, 1],
+  >>> #        [1, 1]]
+  >>> # sum_row = [2, 2], sum_col = [2, 2], true_positives = [1, 1]
+  >>> # iou = true_positives / (sum_row + sum_col - true_positives))
+  >>> # iou = [0.33, 0.33]
+  >>> m = tf.keras.metrics.IoU(num_classes=2, target_class_id=0)
+  >>> m.update_state([0, 0, 1, 1], [0, 1, 0, 1])
+  >>> m.result().numpy()
+  0.33333334
+
+  >>> m.reset_state()
+  >>> m.update_state([0, 0, 1, 1], [0, 1, 0, 1],
+  ...                sample_weight=[0.3, 0.3, 0.3, 0.1])
+  >>> # cm = [[0.3, 0.3],
+  >>> #        [0.3, 0.1]]
+  >>> # sum_row = [0.6, 0.4], sum_col = [0.6, 0.4], true_positives = [0.3, 0.1]
+  >>> # iou = [0.33, 0.14]
+  >>> m.result().numpy()
+  0.33
+
+  Usage with `compile()` API:
+
+  ```python
+  model.compile(
+    optimizer='sgd',
+    loss='mse',
+    metrics=[tf.keras.metrics.IoU(num_classes=2, target_class_id=0)])
+  ```
+  """
+
+  def __init__(self, num_classes, target_class_ids, name=None, dtype=None):
+    super(IoU, self).__init__(
+        name=name,
+        num_classes=num_classes,
+        dtype=dtype,
+    )
+    if max(target_class_ids) >= num_classes:
+      raise ValueError(
+          f'Target class id {max(target_class_ids)} is out of range, which is '
+          f'[{0}, {num_classes}).')
+    self.target_class_ids = target_class_ids
+
+  def result(self):
+    """Compute the intersection-over-union via the confusion matrix."""
+    sum_over_row = tf.cast(
+        tf.reduce_sum(self.total_cm, axis=0), dtype=self._dtype)
+    sum_over_col = tf.cast(
+        tf.reduce_sum(self.total_cm, axis=1), dtype=self._dtype)
+    true_positives = tf.cast(
+        tf.linalg.tensor_diag_part(self.total_cm), dtype=self._dtype)
+
+    # sum_over_row + sum_over_col =
+    #     2 * true_positives + false_positives + false_negatives.
+    denominator = sum_over_row + sum_over_col - true_positives
+
+    iou = tf.math.divide_no_nan(true_positives, denominator)
+
+    return tf.gather(iou, self.target_class_ids, name='iou')
+
+  def get_config(self):
+    config = {
+        'num_classes': self.num_classes,
+        'target_class_ids': self.target_class_ids,
+    }
+    base_config = super(IoU, self).get_config()
+    return dict(list(base_config.items()) + list(config.items()))
+
+
 @keras_export('keras.metrics.MeanIoU')
-class MeanIoU(Metric):
+class MeanIoU(_BaseIOU):
   """Computes the mean Intersection-Over-Union metric.
 
   Mean Intersection-Over-Union is a common evaluation metric for semantic image
@@ -3048,52 +3213,11 @@ class MeanIoU(Metric):
   """
 
   def __init__(self, num_classes, name=None, dtype=None):
-    super(MeanIoU, self).__init__(name=name, dtype=dtype)
-    self.num_classes = num_classes
-
-    # Variable to accumulate the predictions in the confusion matrix.
-    self.total_cm = self.add_weight(
-        'total_confusion_matrix',
-        shape=(num_classes, num_classes),
-        initializer=tf.compat.v1.zeros_initializer)
-
-  def update_state(self, y_true, y_pred, sample_weight=None):
-    """Accumulates the confusion matrix statistics.
-
-    Args:
-      y_true: The ground truth values.
-      y_pred: The predicted values.
-      sample_weight: Optional weighting of each example. Defaults to 1. Can be a
-        `Tensor` whose rank is either 0, or the same rank as `y_true`, and must
-        be broadcastable to `y_true`.
-
-    Returns:
-      Update op.
-    """
-
-    y_true = tf.cast(y_true, self._dtype)
-    y_pred = tf.cast(y_pred, self._dtype)
-
-    # Flatten the input if its rank > 1.
-    if y_pred.shape.ndims > 1:
-      y_pred = tf.reshape(y_pred, [-1])
-
-    if y_true.shape.ndims > 1:
-      y_true = tf.reshape(y_true, [-1])
-
-    if sample_weight is not None:
-      sample_weight = tf.cast(sample_weight, self._dtype)
-      if sample_weight.shape.ndims > 1:
-        sample_weight = tf.reshape(sample_weight, [-1])
-
-    # Accumulate the prediction to current confusion matrix.
-    current_cm = tf.math.confusion_matrix(
-        y_true,
-        y_pred,
-        self.num_classes,
-        weights=sample_weight,
-        dtype=self._dtype)
-    return self.total_cm.assign_add(current_cm)
+    super(MeanIoU, self).__init__(
+        name=name,
+        num_classes=num_classes,
+        dtype=dtype,
+    )
 
   def result(self):
     """Compute the mean intersection-over-union via the confusion matrix."""
@@ -3118,10 +3242,6 @@ class MeanIoU(Metric):
 
     return tf.math.divide_no_nan(
         tf.reduce_sum(iou, name='mean_iou'), num_valid_entries)
-
-  def reset_state(self):
-    backend.set_value(
-        self.total_cm, np.zeros((self.num_classes, self.num_classes)))
 
   def get_config(self):
     config = {'num_classes': self.num_classes}
